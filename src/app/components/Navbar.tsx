773fa0fcb9c7e7af9f10cc2fae342b51e377de18
Sirada U <nan.uthaiwat@gmail.com>
2025-02-04 00:42:05 +0700

    fix: navbar and remove the old one

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,69 +1,80 @@
-"use client";
-import React, { useState } from "react";
-import Image from "next/image";
-import logo from "/public/White.png";
-import Link from "next/link";
+import React from "react";
 import { NavList } from "../constants/navList";
+import Link from "next/link";
+import logo from "/public/White.png";
+import Image from "next/image";
 
-const Navbar: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+export default function Navbar() {
   return (
-    <>
-      <div className="sticky top-0 bg-[#720606] z-50 flex justify-between">
-        <div className="flex m-2 p-2 text-sm text-white font-bold">
-          <Image src={logo} alt="smcu" className="mr-2 h-6 w-fit" />
-          AMSA-Thailand
-        </div>
-        <div className="m-2 p-2 text-white text-sm rounded-full max-sm:hidden">
-          {NavList.map((item,itemIndex) => (
-            
-            <Link href={item.ref}key={itemIndex} className="m-2 hover:font-bold">{item.name}</Link>
-          ))}
-        </div>
-        {/* mobile */}
-        <div className="md:hidden w-5/6 rounded-r-lg">
-          <button
-            className="text-black dark:text-white focus:outline-none "
-            onClick={toggleMenu}
-          >
-            <svg
-              className="w-6 h-6 block"
-              style={{ position: "fixed", right: 20 }}
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="white"
+    <div className="drawer z-50 sticky top-0">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+        {/* Navbar */}
+        <div className="navbar bg-[#720606] w-full text-white">
+          <div className="flex-none lg:hidden">
+            <label
+              htmlFor="my-drawer-3"
+              aria-label="open sidebar"
+              className="btn btn-square btn-ghost"
             >
-              {isOpen ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block h-6 w-6 stroke-current"
+              >
                 <path
-                  // strokeLinecap="round"
-                  // strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              ) : (
-                <path
-                  // strokeLinecap="round"
-                  // strokeLinejoin="round"
-                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M4 6h16M4 12h16M4 18h16"
-                />
-              )}
-            </svg>
-          </button>
-        </div>
-        {isOpen && (
-          <div className="transition ease-in-out dealay-150 left-0 right-0 rounded-lg flex flex-col items-center shadow-md bg-white fixed md:hidden my-12 mx-5">
-            {NavList.map((item) => (
-              <Link href={item.ref} className="m-2 hover:font-bold">{item.name}</Link>
-            ))}
+                ></path>
+              </svg>
+            </label>
           </div>
-        )}
+          <Image src={logo} alt="smcu" className="w-12 h-fit" />
+          <div className="mx-2 flex-1 px-2">AMSA-Thailand</div>
+          <div className="hidden flex-none lg:block">
+            <ul className="menu menu-horizontal">
+              {/* Navbar menu content here */}
+              <li>
+                <Link className="hover:font-bold" href='/#welcome'>Home</Link>
+              </li>
+              <li>
+                <Link className="hover:font-bold" href='/#resources'>Resources</Link>
+              </li>
+              <li>
+                <Link className="hover:font-bold" href='/#teams'>Teams</Link>
+              </li>
+              <li>
+                <Link className="hover:font-bold" href='./article'>Article</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
-    </>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-3"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-base-200 min-h-full w-80 p-4">
+          {/* Sidebar content here */}
+          <li>
+                <Link href='/#welcome'>Home</Link>
+              </li>
+              <li>
+                <Link href='/#resources'>Resources</Link>
+              </li>
+              <li>
+                <Link href='/#teams'>Teams</Link>
+              </li>
+              <li>
+                <Link href='./article'>Article</Link>
+              </li>
+        </ul>
+      </div>
+    </div>
   );
-};
-export default Navbar;
+}
---
+++ src/app/components/eNavbar.tsx
@@ -0,0 +1,69 @@
+// "use client";
+// import React, { useState } from "react";
+// import Image from "next/image";
+// import logo from "/public/White.png";
+// import Link from "next/link";
+// import { NavList } from "../constants/navList";
+
+// const Navbar: React.FC = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
+//   return (
+//     <>
+//       <div className="sticky top-0 bg-[#720606] z-50 flex justify-between">
+//         <div className="flex m-2 p-2 text-sm text-white font-bold">
+//           <Image src={logo} alt="smcu" className="mr-2 h-6 w-fit" />
+//           AMSA-Thailand
+//         </div>
+//         <div className="m-2 p-2 text-white text-sm rounded-full max-sm:hidden">
+//           {NavList.map((item,itemIndex) => (
+            
+//             <Link href={item.ref}key={itemIndex} className="m-2 hover:font-bold">{item.name}</Link>
+//           ))}
+//         </div>
+//         {/* mobile */}
+//         <div className="md:hidden w-5/6 rounded-r-lg">
+//           <button
+//             className="text-black dark:text-white focus:outline-none "
+//             onClick={toggleMenu}
+//           >
+//             <svg
+//               className="w-6 h-6 block"
+//               style={{ position: "fixed", right: 20 }}
+//               xmlns="http://www.w3.org/2000/svg"
+//               fill="none"
+//               viewBox="0 0 24 24"
+//               stroke="white"
+//             >
+//               {isOpen ? (
+//                 <path
+//                   // strokeLinecap="round"
+//                   // strokeLinejoin="round"
+//                   strokeWidth={2}
+//                   d="M6 18L18 6M6 6l12 12"
+//                 />
+//               ) : (
+//                 <path
+//                   // strokeLinecap="round"
+//                   // strokeLinejoin="round"
+//                   strokeWidth={2}
+//                   d="M4 6h16M4 12h16M4 18h16"
+//                 />
+//               )}
+//             </svg>
+//           </button>
+//         </div>
+//         {isOpen && (
+//           <div className="transition ease-in-out dealay-150 left-0 right-0 rounded-lg flex flex-col items-center shadow-md bg-white fixed md:hidden my-12 mx-5">
+//             {NavList.map((item) => (
+//               <Link href={item.ref} className="m-2 hover:font-bold">{item.name}</Link>
+//             ))}
+//           </div>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
+// export default eNavbar;
